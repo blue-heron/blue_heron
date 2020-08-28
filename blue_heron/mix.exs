6e76f41823053cb008fecdacc0aaaa50d90dc44f
Jon Carstens <jjcarstens@me.com>
2020-08-28 12:32:00 -0600

    prep mix.exs for release

--- blue_heron/mix.exs
+++ blue_heron/mix.exs
@@ -1,27 +1,64 @@
 defmodule BlueHeron.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/smartrent/blue_heron"
+
   def project do
     [
       app: :blue_heron,
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      version: @version,
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      docs: docs(),
+      package: package(),
+      preferred_cli_env: [
+        docs: :docs,
+        "hex.build": :docs,
+        "hex.publish": :docs
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
       {:ex_bin, "~> 0.4"},
+      {:ex_doc, "~> 0.22", only: :docs, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Elixir BLE library to communicate with Bluetooth modules"
+  end
+
+  defp docs() do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @source_url,
+        "Bluetooth Core Specification v5.2" =>
+          "https://www.bluetooth.org/docman/handlers/downloaddoc.ashx?doc_id=478726"
+      }
     ]
   end
 end
