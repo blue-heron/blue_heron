3527ed0e13ab12213b33195ca01f28842c06352a
Connor Rigby <connorr@hey.com>
2020-09-14 13:45:08 -0700

    Add Wilink8 uploader. This doesn't work yet, but i think it's close

--- blue_heron/lib/blue_heron/uploader.ex
+++
@@ -1,3 +0,0 @@
-defmodule BlueHeron.BTS.Uploader do
-
-end
---
+++ blue_heron/lib/blue_heron/wilink_8.ex
@@ -0,0 +1,69 @@
+defmodule BlueHeron.Wilink8 do
+  require Logger
+
+  def init do
+    bts = BlueHeron.BTS.decode_file!("/lib/firmware/ti-connectivity/TIInit_11.8.32.bts")
+
+    {:ok, pid} = Circuits.UART.start_link()
+    gpio_bt_en = 60
+    {:ok, pin_ref} = Circuits.GPIO.open(gpio_bt_en, :output)
+    Circuits.GPIO.write(pin_ref, 0)
+    Process.sleep(5)
+    Circuits.GPIO.write(pin_ref, 1)
+    Process.sleep(200)
+
+    :ok =
+      Circuits.UART.open(pid, "ttyS3",
+        speed: 3_000_000,
+        framing: BlueHeronTransportUART.Framing,
+        active: false,
+        flow_control: :hardware
+      )
+
+    state = %{pin_ref: pin_ref, pid: pid}
+    upload(bts.actions, state)
+  end
+
+  def upload([%{type: :action_remarks, data: remark} | rest], state) do
+    Logger.info("TEXAS Remark: #{remark}")
+    upload(rest, state)
+  end
+
+  def upload([%{type: :action_send_command, data: <<1, _::binary>> = packet} | rest], state) do
+    Logger.info("sending HCI packet: #{inspect(packet, base: :hex, limit: :infinity)}")
+    :ok = Circuits.UART.write(state.pid, packet)
+    upload(rest, state)
+  end
+
+  def upload(
+        [%{type: :action_wait_event, data: %{msec: timeout, wait_data: wait_data}} | rest],
+        state
+      ) do
+    case Circuits.UART.read(state.pid, timeout) do
+      {:ok, ^wait_data} ->
+        upload(rest, state)
+
+      {:ok, bad} ->
+        Logger.error(
+          "Bad data: #{inspect(bad, base: :hex, limit: :infinity)}\nexpected: #{
+            inspect(wait_data, limit: :infinity, base: :hex)
+          }"
+        )
+
+        {:error, bad}
+
+      error ->
+        error
+    end
+  end
+
+  def upload([%{type: :action_serial, data: %{baud: baud, flow: flow}} | rest], state) do
+    Logger.info("not sure what to do with action_serial: #{baud} #{flow}")
+    upload(rest, state)
+  end
+
+  def upload([], state) do
+    Circuits.UART.close(state.pid)
+    :ok
+  end
+end
