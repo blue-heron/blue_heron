0a7c968559c0b9586e283de1fa5d2ba22c8f5b9c
Connor Rigby <connorr@hey.com>
2020-09-16 17:41:17 -0700

    Add create_connection_cancel event

---
+++ blue_heron/lib/blue_heron/hci/commands/le_controller/create_connection_cancel.ex
@@ -0,0 +1,28 @@
+defmodule BlueHeron.HCI.Command.LEController.CreateConnectionCancel do
+  use BlueHeron.HCI.Command.LEController, ocf: 0x000E
+  defparameters([])
+
+  defimpl BlueHeron.HCI.Serializable do
+    def serialize(%{opcode: opcode}) do
+      <<opcode::binary, 0::8, "">>
+    end
+  end
+
+  @impl BlueHeron.HCI.Command
+  def deserialize(<<@opcode::binary, 0::8, "">>) do
+    # This is a pretty useless function because there aren't
+    # any parameters to actually parse out of this, but we
+    # can at least assert its correct with matching
+    %__MODULE__{}
+  end
+
+  @impl BlueHeron.HCI.Command
+  def deserialize_return_parameters(<<status::8>>) do
+    %{status: BlueHeron.ErrorCode.name!(status)}
+  end
+
+  @impl true
+  def serialize_return_parameters(%{status: status}) do
+    <<BlueHeron.ErrorCode.error_code!(status)::8>>
+  end
+end
