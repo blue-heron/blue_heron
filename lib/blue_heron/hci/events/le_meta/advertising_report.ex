7054747fa981ab34e7a89a935e1ee1cec9683894
Connor Rigby <connorr@hey.com>
2024-12-20 11:22:16 -0800

    Add docs to HCI LE_Meta events

--- lib/blue_heron/hci/events/le_meta/advertising_report.ex
+++ lib/blue_heron/hci/events/le_meta/advertising_report.ex
@@ -2,15 +2,10 @@ defmodule BlueHeron.HCI.Event.LEMeta.AdvertisingReport do
   use BlueHeron.HCI.Event.LEMeta, subevent_code: 0x02
 
   @moduledoc """
-  A struct representing a LE Advertising Report.
-
   > The LE Advertising Report event indicates that one or more Bluetooth devices have responded to
   > an active scan or have broadcast advertisements that were received during a passive scan. The
   > Controller may queue these advertising reports and send information from multiple devices in
   > one LE Advertising Report event.
-  >
-  > This event shall only be generated if scanning was enabled using the LE Set Scan Enable
-  > command. It only reports advertising events that used legacy advertising PDUs.
 
   Reference: Version 5.2, Vol 4, Part E, 7.7.65.2
   """
--- lib/blue_heron/hci/events/le_meta/connection_complete.ex
+++ lib/blue_heron/hci/events/le_meta/connection_complete.ex
@@ -2,23 +2,8 @@ defmodule BlueHeron.HCI.Event.LEMeta.ConnectionComplete do
   use BlueHeron.HCI.Event.LEMeta, subevent_code: 0x01
 
   @moduledoc """
-  The HCI_LE_Connection_Complete event indicates to both of the Hosts forming
-  the connection that a new connection has been created.
-
-  Upon the creation of the connection a Connection_Handle shall be assigned by
-  the Controller, and passed to the Host in this event. If the connection
-  creation fails this event shall be provided to the Host that had issued the
-  HCI_LE_Create_Connection command.
-
-  This event indicates to the Host which issued an HCI_LE_Create_Connection
-  command and received an HCI_Command_Status event if the connection creation
-  failed or was successful.
-
-  The Master_Clock_Accuracy parameter is only valid for a slave. On a master,
-  this parameter shall be set to 0x00.
-
-  Note: This event is not sent if the HCI_LE_Enhanced_Connection_Complete event
-  (see Section 7.7.65.10) is unmasked.
+  > The HCI_LE_Connection_Complete event indicates to both of the Hosts forming
+  > the connection that a new connection has been created.
 
   Reference: Version 5.2, Vol 4, Part E, 7.7.65.1
   """
